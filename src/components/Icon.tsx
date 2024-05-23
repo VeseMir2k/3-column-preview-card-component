5eead23bd7aec0d91762628dc11571152cdafc32
Adam Stępień <vesemir2k@gmail.com>
2024-05-23 04:31:07 +0200

    ✨ (components): Add margin-bottom style to Icon component for better spacing ✨ (components): Add margin-top style to Text component for better spacing ✨ (components): Add margin-top style to Title component for better spacing

--- src/components/Icon.tsx
+++ src/components/Icon.tsx
@@ -1,5 +1,5 @@
 const Icon = ({ icon, alt = 'icon' }: { icon: string; alt?: string }) => {
-  return <img src={icon} alt={alt} />;
+  return <img className="mb-[35px]" src={icon} alt={alt} />;
 };
 
 export default Icon;
--- src/components/Text.tsx
+++ src/components/Text.tsx
@@ -1,5 +1,5 @@
 const Text = ({ text }: { text: string }) => {
-  return <p className="font-lexend_deca text-[15px] font-normal leading-[25px] opacity-75">{text}</p>;
+  return <p className="font-lexend_deca my-[25px] text-[15px] font-normal leading-[25px] opacity-75">{text}</p>;
 };
 
 export default Text;
