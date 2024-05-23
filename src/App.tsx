35402ef5389b373e08d1590a0cc74e7a13b68585
Adam Stępień <vesemir2k@gmail.com>
2024-05-23 02:51:04 +0200

    ✨ (public/assets): Add luxury, sedans, and suvs icons in SVG format to the project 📝 (src/App.tsx): Refactor App component to dynamically render cards based on data from data.json file

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,13 @@
-import suvsIcon from './assets/icon-suvs.svg';
 import Card from './components/Card';
 
-const App = () => {
-  const textContent =
-    'Take an SUV for its spacious interior, power, and versatility. Perfect for your next family vacation and off-road adventures.';
+import data from './data.json';
 
-  return (
-    <div>
-      <Card icon={suvsIcon} alt="suv icon" title="SUVS" text={textContent} />
-    </div>
-  );
+const App = () => {
+  const cards = data.map((item, index) => (
+    <Card icon={item.icon_src} alt={item.alt} title={item.title} text={item.text} key={index} />
+  ));
+
+  return <div>{cards}</div>;
 };
 
 export default App;
